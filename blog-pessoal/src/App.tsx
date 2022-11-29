ceba38656932672e134de871d8540c8648c353e3
CarlosEduardoSilverio <cadusilverio444@gmail.com>
2022-11-29 16:47:37 -0300

    Atualização3

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/cadastrousuario' element={<CadastroUsuario />} />
         </Routes>
       <Footer />
     </Router>

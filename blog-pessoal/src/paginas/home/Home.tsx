44d6bfc7ae4208f4209f01b4658b84d3d52e9179
CarlosEduardoSilverio <cadusilverio444@gmail.com>
2022-11-29 08:39:06 -0300

    Atualização

--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { deflate } from "zlib";
-import './Home.css'
+import  './Home.css'
+import {Grid, Button, Typography} from '@material-ui/core'
+import {Box} from '@mui/material'
 
 function Home(){
     return(
-        <body>
-        <div className="container">
-        <h1 className="fonte mr">Pagina Inicial</h1>
-        <p className="fonte mr">Configurações</p>
-        <p className="fonte mr">Ajuda</p>
-        <p className="fonte">Contato</p>
-        </div>
-        <div className="center">
-        <img className="tamanho" src="https://preview.redd.it/ds8uqr6qgiy61.jpg?auto=webp&s=a81f6922e71f684a2c04af8f837a8c1b2f3f365b" alt="" />
-        </div>
-        </body>
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+</>
     );
 }
 

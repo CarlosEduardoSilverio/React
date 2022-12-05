1261a8bdaa39deb02620beed87a9fa1e8ae0528d
CarlosEduardoSilverio <cadusilverio444@gmail.com>
2022-12-05 12:05:58 -0300

    Nova_Atualização

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -6,6 +6,12 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
   return (
@@ -13,9 +19,18 @@ function App() {
       <Navbar />
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/home' element={<Home />} />
           <Route path='/cadastrousuario' element={<CadastroUsuario />} />
-        </Routes>
+          <Route path='/temas' element={<ListaTema />} />
+          <Route path='/postagem' element={<ListaPostagem/>} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+       </Routes>
       <Footer />
     </Router>
   );

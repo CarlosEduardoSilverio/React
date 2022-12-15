f35415d2d52c56af9afc719b444b45fda3572f99
CarlosEduardoSilverio <cadusilverio444@gmail.com>
2022-12-15 13:50:19 -0300

    BlogFinal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import  './Home.css'
+import {Grid, Button, Typography} from '@material-ui/core'
+import {Box} from '@mui/material'
+import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { TokenState } from "../../store/tokens/tokensReducer";
+
+function Home(){
+    let navigate = useNavigate();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          navigate("/login")
+  
+      }
+  }, [token])
+    return(
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo TextoH' >Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo' >expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalPostagem />
+                        </Box>
+                        <Button variant="outlined" className='botao TextoH'>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
